1a00ca2bb4cbc49409ba99fa61b58b0d2156b543
Vahe Grigoryan <vahegian@gmail.com>
2021-04-01 21:40:14 +0200

    storing in vec

--- src/main.rs
+++ src/main.rs
@@ -26,8 +26,14 @@ fn main() {
     let csvf = fs::read_to_string("./raw/all_events.csv");
     let csvu = csvf.unwrap();
     let mut reader = csv::Reader::from_reader(csvu.as_bytes());
+    let mut data: Vec<Box<RotkiAll>> = vec!();
     for record in reader.deserialize() {
-        let record: RotkiAll = record.unwrap();
-        println!("{:?}", record);
+        // let mut record: RotkiAll = record.unwrap();
+        data.push(Box::new(record.unwrap()));
+        // println!("{:?}", record);
     }
+
+    // let d = data[0];
+
+    println!("{:?}", data[0].net_profit_or_loss.as_ref().unwrap());
 }
